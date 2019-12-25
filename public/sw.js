62e51eb1e5ee9a837abd13c09bee35f761186047
edisonthk <edisonthk@gmail.com>
2019-12-25 16:50:54 +0900

    service workerを追加

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+// service worker
+const CACHE_NAME = 'cache-v3';
+
+// workbox.webpack の InjectManifest が自動生成
+const urlsToCache = (self.__precacheManifest || []).map(item => item.url);
+
+// キャッシュするべきのURLリストを初期化
+const initializeCacheList = () => {
+  return caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(urlsToCache));
+};
+
+// もしリクエストのキャッシュが存在したら、キャッシュを返す。
+// もしなかったら、ネットワークから fetch する。
+const responseIfCached = (request) => {
+  return caches.open(CACHE_NAME)
+    .then(cache => cache.match(request))
+    .then(response => {
+      return response || fetch(request);
+    });
+};
+
+// 使われていないキャッシュを消す
+const removeUnusedCache = () => {
+  return caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if(key !== CACHE_NAME) {
+        return caches.delete(key);
+      }
+    }));
+  });
+};
+
+self.addEventListener('install', (event) => {
+
+  event.waitUntil(initializeCacheList());
+});
+
+self.addEventListener('fetch', (event) => {
+
+  // fetchイベントはrequest時に発火する
+  event.respondWith(responseIfCached(event.request));
+});
+
+self.addEventListener('activate', (event) => {
+
+  event.waitUntil(removeUnusedCache());
+});
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = {
   configureWebpack: config => {
     config.resolve.alias = {
       '@': path.resolve(__dirname, 'src'),
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new InjectManifest({
+          swSrc: './public/sw.js',
+        })
+      );
     }
+
   }
 }
